5d663346d50289ca4a13a262ccf4ae35156289c0
TJB-1837 <antoinemarmol77@gmail.com>
2026-01-19 23:42:20 +0100

    "map with dash app"

--- src/utils/clean_data.py
+++ src/utils/clean_data.py
@@ -3,9 +3,7 @@ from pandas import Series,DataFrame
 import geopandas
 import numpy as np
 
-data = pd.read_csv("data/raw/consommation-annuelle-d-electricite-et-gaz-par-commune.csv")
-
-
+data = pd.read_csv("data/raw/new-consommation-annuelle-d-electricite-et-gaz-par-commune.csv")
 
 def clean_geojson():   
     geo_data_dir = "data\\raw\\datagouv-communes.geojson"
@@ -27,7 +25,16 @@ def clean_geojson():
     # écriture dans un fichier
     idf.to_file("data\\cleaned\\communes_idf.geojson", driver="GeoJSON")
 
+def clean_csv():
+    data = pd.read_csv("data/raw/new-consommation-annuelle-d-electricite-et-gaz-par-commune.csv")
+    df = data[["Année","Code Région","Code Département","Code Commune","Conso totale (MWh)"]]
+    df.to_csv("data/cleaned/codes_et_conso_totale.csv")
+
+def clean_all_data():
+    clean_geojson()
+    clean_csv()
 
 
 if __name__=="__main__":
-    clean_geojson()
+    print("Cleaning data")
+    clean_csv()
