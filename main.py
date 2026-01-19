080a6ab001b7a437601d826f38c2525384af3c32
Alex Ptolet <alex.pitolet@edu.esiee.fr>
2026-01-19 09:21:06 +0100

    Histogramme (main+clean_data)

--- main.py
+++ main.py
@@ -1,9 +1,57 @@
-import pandas as pd
+
+#
+# Imports
+#
 import numpy as np
+import pandas as pd
 import plotly.express as px
 
+import dash
+from dash import Dash,dcc,html
+from dash.dependencies import Input,Output
+
+#
+# Data
+#
+
+df = pd.read_csv("data/cleaned/conso_per_region.csv")
+
+#
+# Main
+#
+
 def main():
-    print("Hello from data-project!")
-    
-if __name__ == "__main__":
+    app = dash.Dash(__name__) # (3)
+
+    fig = px.bar(df, x="Nom Région", y="Conso moyenne (MWh)", color="Nom Région") # (4)
+
+
+    app.layout = html.Div(children=[
+
+                            html.H1(children=f'Conso per region ',
+                                        style={'textAlign': 'center', 'color': '#7FDBFF'}), # (5)
+
+                            dcc.Graph(
+                                id='graph1',
+                                figure=fig
+                            ), # (6)
+
+                            html.Div(children=f'''
+                                Ce graphe montre la consommation moyenne d'électricité en MWh par 
+                                région, lissée par nombre de site pt de livraison) au sein de cette dernière.
+                                Chaque région possède sa couleur et la taille d'une barre dans l'histogramme
+                                est proportionnelle à la consommation de la région associée
+                                Passe la souris par dessus pour plus de détail.
+                            '''), # (7)
+
+    ]
+    )
+
+    #
+    # RUN APP
+    #
+
+    app.run(debug=True) # (8)
+
+if __name__ == '__main__':
     main()
--- src/utils/clean_data.py
+++ src/utils/clean_data.py
@@ -3,7 +3,7 @@ import numpy as np
 
 df_raw=pd.read_csv("data/raw/dataset/consommation-annuelle-d-electricite-et-gaz-par-commune.csv", sep=";")  
 
-cols1 = df_raw.loc[:,"OPERATEUR" : "Nom Commune"].columns
+cols1 = df_raw.loc[:,"OPERATEUR" : "Code Commune"].columns
 cols2 = df_raw.loc[:,"Code Département" : "CODE GRAND SECTEUR"].columns
 cols3 = df_raw.loc[:,"Conso totale (MWh)": "Conso moyenne (MWh)"].columns
 cols4 = df_raw.loc[:,"Nombre d'habitants":"Superficie des logements >100 m2"].columns
@@ -19,4 +19,15 @@ cols5 = ["Taux de chauffage électrique"]
 
 df = df_raw.loc[:,cols1.tolist()+ cols2.tolist()+ cols3.tolist()+ cols4.tolist()+ cols5]
 print(df.info())
-df.to_csv("data/cleaned/cleanedDataset.csv")
+
+conso_moy_per_region ={}
+for i,region in enumerate(df["Code Région"].sort_values().unique()) : 
+    df_region = df[df["Code Région"]==region]
+    mean_conso = df_region["Conso moyenne (MWh)"].mean()
+    conso_moy_per_region[df_region["Nom Région"].mode()[0]] = (float)(mean_conso) #ajouter le mode au prétraitement ? 
+
+print(conso_moy_per_region)
+
+df_conso_moy_per_region = pd.DataFrame.from_dict(conso_moy_per_region, orient= 'index', columns=["Conso moyenne (MWh)"])
+df_conso_moy_per_region.index.name = "Nom Région"
+conso_moy_per_region_csv =df_conso_moy_per_region.to_csv("data/cleaned/conso_per_region.csv")
