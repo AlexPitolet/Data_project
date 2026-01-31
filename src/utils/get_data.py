485315a63e56e249d600ede15ee9419a9fe17a30
AlexPitolet <alex.pitolet@gmail.com>
2026-01-31 13:30:12 +0100

    patch divers

--- src/utils/get_data.py
+++ src/utils/get_data.py
@@ -4,9 +4,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 RAW_DATA_DIR = os.path.join(BASE_DIR, "data", "raw")
 
-#RAW_DATA_DIR = "/data/raw"
 #RAW_DATA_DIR = "data/raw"
-NB_DATA = 3 # à modifier en fonction du nb de fichier à obtenir au final dans data/raw 
+NB_DATA = 4 # à modifier en fonction du nb de fichier à obtenir au final dans data/raw 
 
 def get_geojson():
     ### Get France departements borders geojson
